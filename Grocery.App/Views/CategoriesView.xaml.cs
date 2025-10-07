6e856d6c35eb356fecb40429849ff19fc4160652
jcmvdb <s1215501@student.windesheim.nl>
2025-10-07 23:10:07 +0200

    Connected the view and viewModel

--- Grocery.App/Views/CategoriesView.xaml.cs
+++ Grocery.App/Views/CategoriesView.xaml.cs
@@ -3,13 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Grocery.App.ViewModels;
 
 namespace Grocery.App.Views;
 
 public partial class CategoriesView : ContentPage
 {
-    public CategoriesView()
+    public CategoriesView(CategoriesViewModel categoriesViewModel)
     {
         InitializeComponent();
+        BindingContext = categoriesViewModel;
+    }
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+        if (BindingContext is CategoriesViewModel bindingContext)
+        {
+            bindingContext.OnAppearing();
+        }
+    }
+
+    protected override void OnDisappearing()
+    {
+        base.OnDisappearing();
+        if (BindingContext is CategoriesViewModel bindingContext)
+        {
+            bindingContext.OnDisappearing();
+        }
     }
 }
