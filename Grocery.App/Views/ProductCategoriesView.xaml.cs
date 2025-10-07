6a3475a6eb80b8e82a3bb64258c4c17eed706550
jcmvdb <s1215501@student.windesheim.nl>
2025-10-07 22:36:33 +0200

    Created the views

---
+++ Grocery.App/Views/ProductCategoriesView.xaml.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grocery.App.Views;
+
+public partial class ProductCategoriesView : ContentPage
+{
+    public ProductCategoriesView()
+    {
+        InitializeComponent();
+    }
+}
