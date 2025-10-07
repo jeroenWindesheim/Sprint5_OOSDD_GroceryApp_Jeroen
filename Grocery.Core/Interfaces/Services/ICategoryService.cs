f450f96db093b360c2353d971f04b6421b91367d
jcmvdb <s1215501@student.windesheim.nl>
2025-10-07 22:36:18 +0200

    Created the services and interfaces

---
+++ Grocery.Core/Interfaces/Services/ICategoryService.cs
@@ -0,0 +1,9 @@
+using Grocery.Core.Models;
+
+namespace Grocery.Core.Interfaces.Services;
+
+public interface ICategoryService
+{
+    public List<Category> GetAll();
+    public Category? Get(int id);
+}
---
+++ Grocery.Core/Services/ProductCategoryService.cs
@@ -0,0 +1,8 @@
+using Grocery.Core.Interfaces.Services;
+
+namespace Grocery.Core.Services;
+
+public class ProductCategoryService : IProductCategoryService
+{
+    
+}
